43bd976ff8ecb15126d295b35bc59fcb3d764e83
Keijiro Takahashi <keijiro@gmail.com>
2020-12-29 14:58:10 +0900

    widescreen

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -22,15 +22,16 @@ sealed class CameraController : MonoBehaviour
 
     #region Compile-time constants
 
-    public const int Size = 416;
+    public const int Width = 640;
+    public const int Height = 360;
 
     #endregion
 
     #region Internal objects
 
     WebCamTexture _webcam;
-    RenderTexture _cropped;
     ComputeBuffer _buffer;
+    RenderTexture _mask;
     IWorker _worker;
 
     #endregion
@@ -41,27 +42,23 @@ sealed class CameraController : MonoBehaviour
     {
         _webcam = new WebCamTexture();
         _webcam.Play();
-
-        _preview.texture = _cropped = new RenderTexture(Size, Size, 0);
-        _buffer = new ComputeBuffer(Size * Size * 3, sizeof(float));
-
+        _preview.texture = _webcam;
+        _buffer = new ComputeBuffer(Width * Height * 3, sizeof(float));
         _worker = ModelLoader.Load(_model).CreateWorker();
     }
 
     void OnDisable()
     {
         _buffer?.Dispose();
-        _buffer = null;
-
         _worker?.Dispose();
+        _buffer = null;
         _worker = null;
     }
 
     void OnDestroy()
     {
         if (_webcam != null) Destroy(_webcam);
-        if (_cropped != null) Destroy(_cropped);
-        if (_overlay.texture != null) Destroy(_overlay.texture);
+        if (_mask != null) Destroy(_mask);
     }
 
     void Update()
@@ -70,26 +67,21 @@ sealed class CameraController : MonoBehaviour
         if (_worker.scheduleProgress >= 1)
         {
             // Replace the overlay texture with the output.
-            if (_overlay.texture != null) Destroy(_overlay.texture);
+            if (_mask != null) Destroy(_mask);
             var output = _worker.PeekOutput("float_segments");
-            using (var segs = output.Reshape(new TensorShape(1, 26, 26, 1)))
-                _overlay.texture = segs.ToRenderTexture();
+            using (var segs = output.Reshape(new TensorShape(1, 23, 40, 1)))
+                _overlay.texture = _mask = segs.ToRenderTexture();
         }
 
-        // Input image cropping
-        var aspect = (float)_webcam.height / _webcam.width;
-        var scale = new Vector2(aspect, 1);
-        var offset = new Vector2(aspect / 2, 0);
-        Graphics.Blit(_webcam, _cropped, scale, offset);
-
         // Image to tensor conversion
-        _converter.SetTexture(0, "_Image", _cropped);
+        _converter.SetTexture(0, "_Texture", _webcam);
         _converter.SetBuffer(0, "_Tensor", _buffer);
-        _converter.SetInt("_Width", Size);
-        _converter.Dispatch(0, Size / 8, Size / 8, 1);
+        _converter.SetInt("_Width", Width);
+        _converter.SetInt("_Height", Height);
+        _converter.Dispatch(0, Width / 8, Height / 8, 1);
 
         // New task scheduling
-        using (var tensor = new Tensor(1, Size, Size, 3, _buffer))
+        using (var tensor = new Tensor(1, Height, Width, 3, _buffer))
         {
             var inputs = new Dictionary<string, Tensor> {{ "sub_2", tensor }};
             _worker.Execute(inputs);
